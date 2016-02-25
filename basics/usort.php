3b4066168a78de89b37633f8f63a51b79ebb39c2
hackingangle <hackingangle@gmail.com>
2016-02-25 20:34:12 +0800

    sort by user defined compare function

---
+++ basics/usort.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * sort 分为9种
+ * sort,asort,ksort
+ * rsort,arsort,krsort
+ * usort,uasort,uksort
+ */
+
+$users = [
+    [
+        'username' => 'zhangsan',
+        'age' => 32,
+    ],
+    [
+        'username' => 'lisi',
+        'age' => 26,
+    ],
+    [
+        'username' => 'wangermazi',
+        'age' => 90,
+    ],
+    [
+        'username' => 'zhaosixiaojie',
+        'age' => 20,
+    ]
+];
+
+function cmpAge($prefix, $suffix)
+{
+    echo $prefix['age'], ',', $suffix['age'], "\n";
+    $attr = 'age';
+    if ($prefix[$attr] > $suffix[$attr]) {
+        return true;
+    }
+    if ($prefix[$attr] < $suffix[$attr]) {
+        return false;
+    }
+    return 0;
+}
+var_dump($users);
+usort($users, 'cmpAge');
+var_dump($users);
